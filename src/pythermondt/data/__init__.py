5e10eae01db5d2d4aaae8cf8415114e9ba2cc749
Julfried <julian.grimm8@gmail.com>
2024-08-07 16:53:22 +0200

    v2 WIP

--- src/pythermondt/data/__init__.py
+++ src/pythermondt/data/__init__.py
@@ -1,3 +1,4 @@
 # from .data_container import DataContainer
-from .datacontainer import DataContainer
+# from .datacontainer import DataContainer
+from .datacontainerv2 import DataContainer
 from .thermo_dataset import ThermoDataset
---
+++ src/pythermondt/data/datacontainerv2/dataset.py
@@ -0,0 +1,6 @@
+from .attributes import _Attributes
+
+class _Datasets:
+    def __init__(self):
+        self.__datasets: dict ={}
+        self.__atttributes = _Attributes()

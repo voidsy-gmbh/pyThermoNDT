cc92ff070eca28e6b51ce83aedab2a895c411ec4
Julfried <julian.grimm8@gmail.com>
2024-10-01 11:37:04 +0200

    Add a progressbar to aws uploader

--- src/pythermondt/writers/aws_writer.py
+++ src/pythermondt/writers/aws_writer.py
@@ -1,4 +1,5 @@
 import boto3
+import progressbar
 from botocore.exceptions import ClientError
 from ..data import DataContainer
 from .base_writer import BaseWriter
@@ -19,16 +20,46 @@ class AWSWriter(BaseWriter):
         self.__client = boto3_session.client('s3')
 
     def write(self, container: DataContainer , file_name: str):
+        # Specify the destination path
         if self.destination_folder:
             path = "/".join([self.destination_folder, file_name])
 
         else:
             path = file_name
 
+        # Serialize the DataContainer to a HDF5 file
+        file_obj = container.serialize_to_hdf5()
+
+        # Progress bar
+        widgets = [
+            f"\033[1mUploading file: {file_name}\033[0m ",  # Bold file name
+            progressbar.Percentage(),
+            ' ', progressbar.Bar(marker='\033[92m■\033[0m', left='|', right='|', fill=' '),
+            ' ', progressbar.DataSize(), ' of ', progressbar.DataSize(variable='max_value'),
+            ' ', progressbar.FileTransferSpeed(),
+            ' ', progressbar.ETA()
+        ]
+        bar = progressbar.ProgressBar(
+            max_value=file_obj.getbuffer().nbytes, 
+            widgets=widgets,
+            enable_colors=False
+        )
+
+         # Callback for progress bar
+        class ProgressCallback:
+            def __init__(self, bar):
+                self.bar = bar
+                self._seen_so_far = 0
+
+            def __call__(self, bytes_amount):
+                self._seen_so_far += bytes_amount
+                self.bar.update(self._seen_so_far)
+
         # Try to upload the file
         try:
-            self.__client.upload_fileobj(container.serialize_to_hdf5(), self.bucket, path)
-            print("Upload successful")
+            self.__client.upload_fileobj(file_obj, self.bucket, path, Callback=ProgressCallback(bar))
+            bar.finish()
+
         except ClientError as e:
             error_code = e.response['Error']['Code']
             if error_code in ['InvalidAccessKeyId', 'SignatureDoesNotMatch', 'AuthFailure', 'InvalidSecurity', 'InvalidToken']:

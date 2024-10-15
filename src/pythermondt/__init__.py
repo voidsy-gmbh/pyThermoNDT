c88d6e1c9411df3fb16c4433db5ec78d745d5b7c
Julfried <julian.grimm8@gmail.com>
2024-10-15 11:10:01 +0200

    Update TODOS

--- src/pythermondt/__init__.py
+++ src/pythermondt/__init__.py
@@ -2,3 +2,5 @@ from .data import DataContainer, ThermoDataset
 from .transforms import normalization, utils, preprocessing
 
 # TODO: Implement logging instead of print statements
+# TODO: Implement async data loading
+# TODO: Implement multi threading for data loading / writing

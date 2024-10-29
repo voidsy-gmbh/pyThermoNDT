34835d969bf96f96aee603d1a3cc38cabd189a39
Julfried <julian.grimm8@gmail.com>
2024-10-29 09:37:28 +0100

    Move to version 0.0.4

--- src/pythermondt/__init__.py
+++ src/pythermondt/__init__.py
@@ -8,4 +8,3 @@ from .__pkginfo__ import __version__
 # TODO: Improve visualization in data container
 # TODO: Add units to thermo container format (update unified format)
 # TODO: Add more tests
-# TODO: Add a custom Subset to be able to apply different transforms to training and test sets

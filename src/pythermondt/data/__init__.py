e4d07c1032ff5f2a100a507b096675f7f3359979
Julfried <julian.grimm8@gmail.com>
2024-11-05 11:50:12 +0100

    Add units to data module

--- src/pythermondt/data/__init__.py
+++ src/pythermondt/data/__init__.py
@@ -1,4 +1,5 @@
 from .datacontainer import DataContainer, ThermoContainer
 from .thermo_dataset import ThermoDataset
 from .thermo_subset import ThermoSubset
+from .units import UNITS
 from .utils import random_split
---
+++ src/pythermondt/data/units/units.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import TypedDict
+
+class UnitInfo(TypedDict):
+    '''TypedDict for unit information.'''
+    name: str
+    quantity: str
+    symbol: str
+
+UNITS = {
+    # Temperature
+    "Kelvin": UnitInfo(name="Kelvin", quantity="Temperature", symbol="K"),
+    "Celsius": UnitInfo(name="Celsius", quantity="Temperature", symbol="°C"),
+
+    # Time units
+    "second": UnitInfo(name="second", quantity="Time", symbol="s"),
+    "millisecond": UnitInfo(name="millisecond", quantity="Time", symbol="ms"),
+
+    # Generic
+    "dimensionless": UnitInfo(name="dimensionless", quantity="dimensionless", symbol="1"),
+
+    # Raw data
+    "arbitrary": UnitInfo(name="arbitrary", quantity="arbitrary", symbol="a. u."),
+}

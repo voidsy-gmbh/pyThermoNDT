8f6e2ff02a57b4f2e0b95025fc5c61bee7c447d0
Julfried <julian.grimm8@gmail.com>
2024-11-05 13:37:14 +0100

    Make units a static class

--- src/pythermondt/data/units/units.py
+++ src/pythermondt/data/units/units.py
@@ -7,18 +7,31 @@ class UnitInfo(TypedDict):
     quantity: str
     symbol: str
 
-class Units(Enum):
-    '''Enum for all units inside pythermondt.'''
+class Units:
+    '''Container for all units inside pythermondt.'''
     # Temperature
-    kelvin = UnitInfo(name="kelvin", quantity="temperature", symbol="K"),
-    celsius = UnitInfo(name="celsius", quantity="temperature", symbol="°C"),
+    kelvin = UnitInfo(name="kelvin", quantity="temperature", symbol="K")
+    celsius = UnitInfo(name="celsius", quantity="temperature", symbol="°C")
     
     # Time 
-    second = UnitInfo(name="second", quantity="time", symbol="s"),
-    millisecond = UnitInfo(name="millisecond", quantity="time", symbol="ms"),
+    second = UnitInfo(name="second", quantity="time", symbol="s")
+    millisecond = UnitInfo(name="millisecond", quantity="time", symbol="ms")
 
     # Generic
-    dimensionless = UnitInfo(name="dimensionless", quantity="dimensionless", symbol="1"),
+    dimensionless = UnitInfo(name="dimensionless", quantity="dimensionless", symbol="1")
     arbitrary = UnitInfo(name="arbitrary", quantity="arbitrary", symbol="a. u.")
 
-    # Add more units here if needed
+    # Add more units here as needed
+
+    def __init__(self):
+        raise TypeError("This class is static and should not be instantiated.")
+
+# Test
+if __name__ == "__main__":
+    print(type(Units.kelvin))
+    print(Units.kelvin)
+    print(Units.celsius)
+    print(Units.second)
+    print(Units.millisecond)
+    print(Units.dimensionless)
+    print(Units.arbitrary)

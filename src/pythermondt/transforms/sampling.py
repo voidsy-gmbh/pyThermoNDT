1aed0c83a2c7e978d35a5325202c863ea8318d65
Julfried <julian.grimm8@gmail.com>
2024-11-18 17:41:48 +0100

    Implement index range Selection

--- src/pythermondt/transforms/sampling.py
+++ src/pythermondt/transforms/sampling.py
@@ -1,6 +1,6 @@
 from .utils import ThermoTransform
 from ..data import DataContainer
-from typing import Sequence
+from typing import Sequence, Optional
 
 class SelectFrames(ThermoTransform):
     """Select a subset of frames from the data container specified by a single index or a list of indices."""
@@ -25,6 +25,40 @@ class SelectFrames(ThermoTransform):
         tdata = tdata[..., self.frame_indices]
         domain_values = domain_values[..., self.frame_indices]
         excitation_signal = excitation_signal[..., self.frame_indices]
+
+        # Update Container and return
+        container.update_datasets(("/Data/Tdata", tdata), ("/MetaData/DomainValues", domain_values), ("/MetaData/ExcitationSignal", excitation_signal))
+        return container
+
+class SelectFrameRange():
+    """Select a range of frames from the data container."""
+    def __init__(self, start: Optional[int] = None, end: Optional[int] = None):
+        """Select a range of frames from the data container.
+
+        Parameters:
+            start (Optional[int]): Start index of the frame range. Default is None, which means the start index is 0.
+            end (Optional[int]): End index of the frame range. Default is None, which means the end index is the last frame.
+        """
+        self.start = start
+        self.end = end
+
+    def forward(self, container: DataContainer) -> DataContainer:
+        # Extract Datasets
+        tdata, domain_values, excitation_signal = container.get_datasets("/Data/Tdata", "/MetaData/DomainValues", "/MetaData/ExcitationSignal")
+
+        # Check if frame range is valid
+        if self.start is not None and (self.start < 0 or self.start >= tdata.shape[-1]):
+            raise ValueError(f"Invalid start index. Start index must be in the range [0, {tdata.shape[-1] - 1}].")
+        
+        if self.end is not None and (self.end < 0 or self.end >= tdata.shape[-1]):
+            raise ValueError(f"Invalid end index. End index must be in the range [0, {tdata.shape[-1] - 1}].")
+
+        # Select Frames
+        start = self.start if self.start is not None else 0
+        end = self.end if self.end is not None else tdata.shape[-1]
+        tdata = tdata[..., start:end]
+        domain_values = domain_values[..., start:end]
+        excitation_signal = excitation_signal[..., start:end]
 
         # Update Container and return
         container.update_datasets(("/Data/Tdata", tdata), ("/MetaData/DomainValues", domain_values), ("/MetaData/ExcitationSignal", excitation_signal))

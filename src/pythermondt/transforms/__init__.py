17ea5d7c9f4d11d19d11bef94d5b54c2c4844e3b
Julfried <51880314+Julfried@users.noreply.github.com>
2024-11-19 10:28:26 +0100

    Frame selection transforms for data in time domain (#11)
    * Update version string
    
    * Implement index based sampling
    
    * refactor: improve DataContainer API with more consistent patterns and better type safety (#10)
    
    * Update version string
    
    * rename get_datasets and get_groups to get_all_datasets and get_all_groups to better show the intent of the function
    
    * Add get datasets to retrieve multiple datasets in 1 line
    
    * Return a tuple instead of a list ==> more pythonic
    
    * Rename the get_all_datasets method to be more explicit what is happening. Also improve docstring
    
    * Use variadics paths for get_datasets
    
    * Employ the same pattern for attributes aswell
    
    * Implement add_datasets to add multiple datasets at once
    
    * Also implement the pattern for update_datasets
    
    * Update tests
    
    * Correct for new container API
    
    * Fix paths
    
    * Add to init
    
    * Remove slice notation because it is uncommon
    
    * Add check for index range
    
    * Rename for better clarity
    
    * Implement index range Selection
    
    * Fix inheritance
    
    * Add transform to init
    
    * call constructor
    
    * Update docstring
    
    * Make last frame inclusive
    
    * Add check to prevent frame selection outside of time domain
    
    * Correct time shift in domain values

--- src/pythermondt/transforms/__init__.py
+++ src/pythermondt/transforms/__init__.py
@@ -1,3 +1,4 @@
 from .preprocessing import ApplyLUT, SubstractFrame
 from .normalization import MinMaxNormalize, MaxNormalize, ZScoreNormalize
 from .utils import Compose, ThermoTransform
+from .sampling import SelectFrames, SelectFrameRange
---
+++ src/pythermondt/transforms/sampling.py
@@ -0,0 +1,80 @@
+from .utils import ThermoTransform
+from ..data import DataContainer
+from typing import Sequence, Optional
+
+class SelectFrames(ThermoTransform):
+    """Select a subset of frames from the data container specified by a single index or a list of indices."""
+    def __init__(self, frame_indices: int | Sequence[int]):
+        """Select a subset of frames from the data container specified by a single index or a list of indices.
+
+        Parameters:
+            frame_indices (int | Sequence[int]): Single index or list of indices to select frames.
+        """
+        super().__init__()
+        self.frame_indices = frame_indices if isinstance(frame_indices, Sequence) else [frame_indices]
+
+    def forward(self, container: DataContainer) -> DataContainer:
+        # Extract Datasets
+        tdata, domain_values, excitation_signal = container.get_datasets("/Data/Tdata", "/MetaData/DomainValues", "/MetaData/ExcitationSignal")
+
+        # Check if frame_indices are valid
+        if any(idx < 0 or idx >= tdata.shape[-1] for idx in self.frame_indices):
+            raise ValueError(f"Invalid frame index. Frame indices must be in the range [0, {tdata.shape[-1] - 1}].")
+        
+        # Check if we are in time domain
+        if container.get_unit("/MetaData/DomainValues")["quantity"] != "time":
+            raise ValueError("SelectFrames transform can only be applied to time domain data.")
+
+        # Select Frames
+        tdata = tdata[..., self.frame_indices]
+        domain_values = domain_values[..., self.frame_indices]
+        excitation_signal = excitation_signal[..., self.frame_indices]
+
+        # Fix time shift in domain values by substracting the first time step
+        domain_values = domain_values - domain_values[0]
+
+        # Update Container and return
+        container.update_datasets(("/Data/Tdata", tdata), ("/MetaData/DomainValues", domain_values), ("/MetaData/ExcitationSignal", excitation_signal))
+        return container
+
+class SelectFrameRange(ThermoTransform):
+    """Select a range of frames from the data container, by specifying their start and end index."""
+    def __init__(self, start: Optional[int] = None, end: Optional[int] = None):
+        """Select a range of frames from the data container, by specifying their start and end index.
+
+        Parameters:
+            start (Optional[int]): Start index of the frame range. Default is None, which means the start index is 0.
+            end (Optional[int]): End index of the frame range, which is inclusiv. Default is None, which means the end index is the last frame.
+        """
+        super().__init__()
+        self.start = start
+        self.end = end
+
+    def forward(self, container: DataContainer) -> DataContainer:
+        # Extract Datasets
+        tdata, domain_values, excitation_signal = container.get_datasets("/Data/Tdata", "/MetaData/DomainValues", "/MetaData/ExcitationSignal")
+
+        # Check if frame range is valid
+        if self.start is not None and (self.start < 0 or self.start >= tdata.shape[-1]):
+            raise ValueError(f"Invalid start index. Start index must be in the range [0, {tdata.shape[-1] - 1}].")
+        
+        if self.end is not None and (self.end < 0 or self.end >= tdata.shape[-1]):
+            raise ValueError(f"Invalid end index. End index must be in the range [0, {tdata.shape[-1] - 1}].")
+        
+        # Check if we are in time domain
+        if container.get_unit("/MetaData/DomainValues")["quantity"] != "time":
+            raise ValueError("SelectFrameRange transform can only be applied to time domain data.")
+
+        # Select Frames (end index is inclusive)
+        start = self.start if self.start is not None else 0
+        end = self.end + 1 if self.end is not None else tdata.shape[-1]
+        tdata = tdata[..., start:end]
+        domain_values = domain_values[..., start:end]
+        excitation_signal = excitation_signal[..., start:end]
+
+        # Fix time shift in domain values by substracting the first time step
+        domain_values = domain_values - domain_values[0]
+
+        # Update Container and return
+        container.update_datasets(("/Data/Tdata", tdata), ("/MetaData/DomainValues", domain_values), ("/MetaData/ExcitationSignal", excitation_signal))
+        return container

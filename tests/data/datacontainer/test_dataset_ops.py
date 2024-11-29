706e7a8d8e2a27f422b33446a2fffd5b91437ad5
Julfried <julian.grimm8@gmail.com>
2024-11-29 17:04:48 +0100

    Test adding dataset finished

--- tests/data/datacontainer/test_dataset_ops.py
+++ tests/data/datacontainer/test_dataset_ops.py
@@ -8,78 +8,52 @@ from pythermondt.data import DataContainer
 from pythermondt.data.datacontainer.node import DataNode
 
 @pytest.fixture
-def dataset_container(empty_container:DataContainer, sample_empty_tensor:Tensor):
+def dataset_container(empty_container:DataContainer):
     empty_container.add_group("/", "testgroup")
     empty_container.add_group("/testgroup", "nestedgroup")
     return empty_container
 
-
-
-@pytest.mark.parametrize("path, name, data", [
-    ("/", "dataset0", None), # add directly to root
-    ("/testgroup", "dataset1", None), # add to a group
-    ("/testgroup/nestedgroup", "dataset2", None), # add to an existing dataset
+# Test adding a dataset to the container
+@pytest.mark.parametrize("data", [
+    pytest.param("sample_tensor"),
+    pytest.param("sample_ndarray"),
+    pytest.param("sample_empty_tensor"),
+    pytest.param("sample_empty_ndarray"),
+    pytest.param(None)
 ])
-def test_add_dataset(dataset_container:DataContainer, data:Optional[Tensor | ndarray], path:str, name:str):
+@pytest.mark.parametrize("path, name", [
+    ("/", "dataset0"), # add directly to root
+    ("/testgroup", "dataset1"), # add to a group
+    ("/testgroup/nestedgroup", "dataset2"), # add to a nested group
+])
+def test_add_dataset(dataset_container:DataContainer, data:str | None, path:str, name:str, request:pytest.FixtureRequest):
+    # Request testdata from the fixtures
+    test_data = request.getfixturevalue(data) if data is not None else None
+
     # Add a dataset
     key = f"{path}/{name}" if path != "/" else f"{path}{name}"
-    dataset_container.add_dataset(path, name, data)
+    dataset_container.add_dataset(path, name, test_data)
 
     # Assertions
-    # Empty dataset
-    if data is None:
-        assert key in dataset_container.nodes.keys()
-        assert isinstance(dataset_container.nodes[key], DataNode)
+    assert key in dataset_container.nodes.keys()
+    assert isinstance(dataset_container.nodes[key], DataNode)
+
+    # Assertions
+    # Empty dataset ==> default value in container is a torch.empty(0) tensor
+    if test_data is None:
         assert torch.equal(dataset_container.nodes[key].data, torch.empty(0)) #type: ignore
     
     # Ndarray
-    elif isinstance(data, ndarray):
-        assert key in dataset_container.nodes.keys()
-        assert isinstance(dataset_container.nodes[key], DataNode)
-        assert torch.equal(dataset_container.nodes[key].data, torch.from_numpy(data)) #type: ignore
+    elif isinstance(test_data, ndarray):
+        assert torch.equal(dataset_container.nodes[key].data, torch.from_numpy(test_data)) #type: ignore
     
     # Tensor
-    elif isinstance(data, Tensor):
-        assert key in dataset_container.nodes.keys()
-        assert isinstance(dataset_container.nodes[key], DataNode)
-        assert torch.equal(dataset_container.nodes[key].data, data) # type:ignore
+    elif isinstance(test_data, Tensor):
+        assert torch.equal(dataset_container.nodes[key].data, test_data) # type:ignore
 
     # Error case
     else:
         assert False
-
-def test_add_dataset_with_ndarray(dataset_ops):
-    path = "/group"
-    name = "dataset2"
-    data = np.array([1, 2, 3])
-    
-    dataset_ops.add_dataset(path, name, data)
-    
-    key = f"{path}/{name}"
-    assert key in dataset_ops.nodes
-    assert isinstance(dataset_ops.nodes[key], DataNode)
-    assert torch.equal(dataset_ops.nodes[key].data, torch.from_numpy(data))
-
-def test_add_dataset_without_data(dataset_ops):
-    path = "/group"
-    name = "dataset3"
-    
-    dataset_ops.add_dataset(path, name)
-    
-    key = f"{path}/{name}"
-    assert key in dataset_ops.nodes
-    assert isinstance(dataset_ops.nodes[key], DataNode)
-    assert dataset_ops.nodes[key].data is None
-
-def test_add_dataset_already_exists(dataset_ops):
-    path = "/group"
-    name = "dataset4"
-    data = torch.tensor([1, 2, 3])
-    
-    dataset_ops.add_dataset(path, name, data)
-    
-    with pytest.raises(KeyError, match=f"Dataset with name: '{name}' at the path: '{path}' already exists."):
-        dataset_ops.add_dataset(path, name, data)
 
 # Only run the tests in this file if it is run directly
 if __name__ == '__main__':

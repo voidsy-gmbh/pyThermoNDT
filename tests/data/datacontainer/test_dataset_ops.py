905f495df991647ad86dd904f34c064d225adaea
Julfried <julian.grimm8@gmail.com>
2024-11-29 17:13:26 +0100

    test getting datasets from the container

--- tests/data/datacontainer/test_dataset_ops.py
+++ tests/data/datacontainer/test_dataset_ops.py
@@ -96,6 +96,46 @@ def test_add_datasets(dataset_container:DataContainer, datasets:dict[str, str | 
         else:
             assert False
 
+# Test getting a dataset from the container
+@pytest.mark.parametrize("data", [
+    pytest.param("sample_tensor"),
+    pytest.param("sample_ndarray"),
+    pytest.param("sample_empty_tensor"),
+    pytest.param("sample_empty_ndarray"),
+])
+@pytest.mark.parametrize("path, name", [
+    ("/", "dataset0"), # get directly from root
+    ("/testgroup", "dataset1"), # get from a group
+    ("/testgroup/nestedgroup", "dataset2"), # get from a nested group
+])
+def test_get_dataset(dataset_container:DataContainer, data:str, path:str, name:str, request:pytest.FixtureRequest):
+    # Request testdata from the fixtures
+    test_data = request.getfixturevalue(data)
+
+    # Add a dataset
+    key = f"{path}/{name}" if path != "/" else f"{path}{name}"
+    dataset_container.add_dataset(path, name, test_data)
+
+    # Get the dataset
+    retrieved_data = dataset_container.get_dataset(key)
+
+    # Assertions
+    # Empty dataset ==> default value in container is a torch.empty(0) tensor
+    if test_data is None:
+        assert torch.equal(retrieved_data, torch.empty(0)) #type: ignore
+    
+    # Ndarray
+    elif isinstance(test_data, ndarray):
+        assert torch.equal(retrieved_data, torch.from_numpy(test_data)) #type: ignore
+    
+    # Tensor
+    elif isinstance(test_data, Tensor):
+        assert torch.equal(retrieved_data, test_data) # type:ignore
+    
+    # Error case
+    else:
+        assert False
+
 # Only run the tests in this file if it is run directly
 if __name__ == '__main__':
     pytest.main(["-v", __file__])

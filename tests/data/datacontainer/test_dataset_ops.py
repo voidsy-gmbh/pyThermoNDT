89b62b999ff04d3d1330c62283d17a56cf20c82f
Julfried <julian.grimm8@gmail.com>
2024-12-02 16:56:49 +0100

    Make tests that should fail an extra test function for better clarity ==> no use of pytest.mark.xail

--- tests/data/datacontainer/test_dataset_ops.py
+++ tests/data/datacontainer/test_dataset_ops.py
@@ -26,9 +26,6 @@ def dataset_container(empty_container:DataContainer):
     ("/", "dataset0"), # add directly to root
     ("/testgroup", "dataset1"), # add to a group
     ("/testgroup/nestedgroup", "dataset2"), # add to a nested group
-    pytest.param("/", "test_dataset0", marks=pytest.mark.xfail(raises=KeyError)), # add to an existing dataset in root
-    pytest.param("/testgroup", "test_dataset1", marks=pytest.mark.xfail(raises=KeyError)), # add to an existing dataset in a group
-    pytest.param("/testgroup/nestedgroup", "test_dataset2", marks=pytest.mark.xfail(raises=KeyError)), # add to an existing dataset in a nested group
 ])
 def test_add_dataset(dataset_container:DataContainer, data:str | None, path:str, name:str, request:pytest.FixtureRequest):
     # Request testdata from the fixtures
@@ -58,6 +55,30 @@ def test_add_dataset(dataset_container:DataContainer, data:str | None, path:str,
     # Error case
     else:
         assert False
+
+# Test adding a dataset that already exists in the container
+@pytest.mark.parametrize("data", [
+    pytest.param("sample_tensor"),
+    pytest.param("sample_ndarray"),
+    pytest.param("sample_empty_tensor"),
+    pytest.param("sample_empty_ndarray"),
+    pytest.param(None)
+])
+@pytest.mark.parametrize("path, name", [
+    pytest.param("/", "test_dataset0",), # add to an existing dataset in root
+    pytest.param("/testgroup", "test_dataset1"), # add to an existing dataset in a group
+    pytest.param("/testgroup/nestedgroup", "test_dataset2"), # add to an existing dataset in a nested group
+])
+def test_add_dataset_existing(dataset_container:DataContainer, data:str, path:str, name:str, request:pytest.FixtureRequest):
+    # Request testdata from the fixtures
+    test_data = request.getfixturevalue(data) if data is not None else None
+
+    # Add a dataset
+    dataset_container.add_dataset(path, name, test_data)
+
+    # Add the same dataset again
+    with pytest.raises(KeyError):
+        dataset_container.add_dataset(path, name, test_data)
 
 # Test adding multiple datasets to the container
 @pytest.mark.parametrize("datasets", [
@@ -111,9 +132,6 @@ def test_add_datasets(dataset_container:DataContainer, datasets:dict[str, str | 
     ("/", "dataset0"), # get directly from root
     ("/testgroup", "dataset1"), # get from a group
     ("/testgroup/nestedgroup", "dataset2"), # get from a nested group
-    pytest.param("/", "non_existent_dataset0", marks=pytest.mark.xfail(raises=KeyError)), # get a non-existent dataset from root
-    pytest.param("/testgroup", "non_existent_dataset1", marks=pytest.mark.xfail(raises=KeyError)), # get a non-existent dataset from a group
-    pytest.param("/testgroup/nestedgroup", "non_existent_dataset2", marks=pytest.mark.xfail(raises=KeyError)), # get a non-existent dataset from a nested group
 ])
 def test_get_dataset(dataset_container:DataContainer, data:str, path:str, name:str, request:pytest.FixtureRequest):
     # Request testdata from the fixtures
@@ -142,6 +160,17 @@ def test_get_dataset(dataset_container:DataContainer, data:str, path:str, name:s
     # Error case
     else:
         assert False
+
+# Test getting a dataset that does not exist in the container
+@pytest.mark.parametrize("path", [
+    "/non_existent_dataset0", # get a non-existent dataset from root
+    "/testgroup/non_existent_dataset1", # get a non-existent dataset from a group
+    "/testgroup/nestedgroup/non_existent_dataset2", # get a non-existent dataset from a nested group
+])
+def test_get_dataset_non_existing(dataset_container:DataContainer, path:str):
+    # Get a non-existent dataset
+    with pytest.raises(KeyError):
+        dataset_container.get_dataset(path)
 
 # Only run the tests in this file if it is run directly
 if __name__ == '__main__':

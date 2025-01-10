2df09a1063cb75be8ec3a7a175d6fdd8f26e9900
Julfried <julian.grimm8@gmail.com>
2025-01-10 18:52:42 +0100

    Rename x and y to height and width

--- src/pythermondt/transforms/augmentation.py
+++ src/pythermondt/transforms/augmentation.py
@@ -4,40 +4,40 @@ from .utils import ThermoTransform
 
 class RandomFlip(ThermoTransform):
     ''' 
-    Randomly flips all frames in the Temperature data (Tdata) of the container along the x-axis, y-axis, or both, based on specified probabilities.
+    Randomly flips all frames in the Temperature data (Tdata) of the container along the height (vertical flip) or width (horizontal flip), or both, based on specified probabilities.
     '''
-    def __init__(self, p_x: float = 0.5, p_y: float = 0.5):
+    def __init__(self, p_height: float = 0.5, p_width: float = 0.5):
         ''' 
-        Initializes the RandomFlip transformation with specified probabilities for flipping along the x-axis and y-axis.
+        Initializes the RandomFlip transformation with specified probabilities for flipping along the height and width directions.
         
         Parameters:
-            p_x (float): Probability of flipping along the x-axis, in the range [0, 1]. Default is 0.5.
-            p_y (float): Probability of flipping along the y-axis, in the range [0, 1]. Default is 0.5.
+            p_height (float): Probability of flipping along the height (vertical flip), in the range [0, 1]. Default is 0.5.
+            p_width (float): Probability of flipping along the width (horizontal flip), in the range [0, 1]. Default is 0.5.
         '''
         super().__init__()
 
-        # Check if p_x and p_y are probabilities
-        if not 0 <= p_x <= 1:
-            raise ValueError("p_x must be in the range [0, 1]")
+        # Check if p_height and p_width are probabilities
+        if not 0 <= p_height <= 1:
+            raise ValueError("p_height must be in the range [0, 1]")
         
-        if not 0 <= p_y <= 1:
-            raise ValueError("p_y must be in the range [0, 1]")
+        if not 0 <= p_width <= 1:
+            raise ValueError("p_width must be in the range [0, 1]")
 
         # Store the probabilities
-        self.p_x = p_x
-        self.p_y = p_y
+        self.p_height = p_height
+        self.p_width = p_width
 
     def forward(self, container: DataContainer) -> DataContainer:
         # Extract the data
         tdata, mask = container.get_datasets("/Data/Tdata", "/GroundTruth/DefectMask")
         
-        # Flip the data along the x-axis if the random number is less than p_x
-        if torch.rand(1).item() < self.p_x:
+        # Flip the data along the height if the random number is less than p_height
+        if torch.rand(1).item() < self.p_height:
             tdata = torch.flip(tdata, [1])
             mask = torch.flip(mask, [1])
         
-        # Flip the data along the y-axis if the random number is less than p_y
-        if torch.rand(1).item() < self.p_y:
+        # Flip the data along the width if the random number is less than p_width
+        if torch.rand(1).item() < self.p_width:
             tdata = torch.flip(tdata, [0])
             mask = torch.flip(mask, [0])
 

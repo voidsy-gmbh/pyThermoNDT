8169c69deb61781a507713b716c787f169e4457d
Julfried <julian.grimm8@gmail.com>
2025-01-14 09:02:11 +0100

    Remove outdated TODO for improving visualization in data container

--- src/pythermondt/__init__.py
+++ src/pythermondt/__init__.py
@@ -7,6 +7,5 @@ from .__pkginfo__ import __version__
 # TODO: Implement logging instead of print statements according to this guide: https://docs.python.org/3/howto/logging.html
 # TODO: Implement async data loading
 # TODO: Implement multi threading for data loading / writing
-# TODO: Improve visualization in data container
 # TODO: Add more tests
 # TODO: Add glob patterns to S3Reader

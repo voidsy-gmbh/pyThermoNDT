c9b5775982f299613edff6375e92731716477f93
Julfried <julian.grimm8@gmail.com>
2025-01-21 14:01:33 +0100

    Remove unused imports in test_serialization_ops.py

--- tests/data/datacontainer/test_serialization_ops.py
+++ tests/data/datacontainer/test_serialization_ops.py
@@ -1,10 +1,4 @@
 from pythermondt.data import DataContainer
-import io
-import numpy as np
-import h5py
-import pytest
-from pythermondt.data.datacontainer.serialization_ops import SerializationOps, DeserializationOps
-from pythermondt.data.datacontainer.node import DataNode, GroupNode, RootNode
 
 def test_serialize_deserialize(filled_container: DataContainer):
     """Test serialization and deserialization of DataContainer."""

31048bc607c43719b0f3da72b84d120ef172d70e
Julfried <julian.grimm8@gmail.com>
2025-01-21 09:49:40 +0100

    Add complex_container fixture for DataContainer with additional attributes

--- tests/data/datacontainer/conftest.py
+++ tests/data/datacontainer/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from torch import Tensor
-from pythermondt.data import DataContainer
+from pythermondt.data import DataContainer, Units
 from pythermondt.data.datacontainer.node import RootNode, GroupNode, DataNode
 
 @pytest.fixture
@@ -37,3 +37,27 @@ def filled_container(empty_container: DataContainer, sample_tensor: Tensor, samp
     empty_container.add_dataset("/TestGroup/NestedGroup", "TestDataset2", sample_eye_tensor)
 
     return empty_container
+
+@pytest.fixture
+def complex_container(filled_container: DataContainer):
+    """Fixture for DataContainer with complex structure. Based on filled_container, with additional atttributes added"""
+    # Define attributes to be added
+    attrs = {
+        "str_attr": "test_string",
+        "int_attr": 42,
+        "float_attr": 3.14,
+        "list_attr": [1, 2, 3],
+        "dict_attr": {"key": "value"},
+        "unit_attr": Units.kelvin
+    }
+
+    # Add various types of attributes to the TestGroup
+    filled_container.add_attributes("/TestGroup", **attrs)
+
+    # Add various types of attributes to the NestedGroup
+    filled_container.add_attributes("/TestGroup/NestedGroup", **attrs)
+
+    # Add various types of attributes to the TestDataset
+    filled_container.add_attributes("/TestGroup/TestDataset", **attrs)
+
+    return filled_container

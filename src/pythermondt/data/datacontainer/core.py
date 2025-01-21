db03e7299c6fc3367ad99bd4b12dac5c322cc08f
Julfried <julian.grimm8@gmail.com>
2025-01-21 11:17:24 +0100

    Implement strict equality comparison for DataContainer instances

--- src/pythermondt/data/datacontainer/core.py
+++ src/pythermondt/data/datacontainer/core.py
@@ -1,11 +1,13 @@
+import torch
 from typing import Optional
 from io import BytesIO
-from .node import RootNode
+from .node import RootNode, GroupNode, DataNode, NodeType
 from .group_ops import GroupOps
 from .dataset_ops import DatasetOps
 from .attribute_ops import AttributeOps
 from .serialization_ops import SerializationOps, DeserializationOps
 from .visualization_ops import VisualizationOps
+from .utils import is_rootnode, is_groupnode, is_datanode
 
 class DataContainer(SerializationOps, DeserializationOps, VisualizationOps, GroupOps, DatasetOps, AttributeOps):
     """
@@ -39,3 +41,56 @@ class DataContainer(SerializationOps, DeserializationOps, VisualizationOps, Grou
             returnstring = returnstring + f"{path}: ({node.name}: {node.type})" + "\n"
 
         return returnstring
+    
+    # Overwrite the __eq__ method to provide a comparison between two DataContainer instances
+    def __eq__(self, other: object) -> bool:
+        """Compare two DataContainers for equality.
+    
+        Containers are equal if they have:
+        1. Same node structure 
+        2. Equal data in all datasets
+        3. Identical attributes for every group and dataset
+
+        **Note**: This implements strict equality. Even with the same initial data, containers 
+        that have undergone stochastic transforms (e.g. GaussianNoise) will not be equal 
+        since their data differs.
+
+        Parameters:
+            other (object): The other object to compare with.
+
+        Returns:
+            bool: True if the two DataContainers are equal, False otherwise.
+        """
+        # Check if the other object is an instance of DataContainer
+        if not isinstance(other, DataContainer):
+            return False
+
+        # Check if node structure is equal
+        if set(self.nodes.keys()) != set(other.nodes.keys()):
+            return False
+        
+        # Compare each node
+        for path, node in self.nodes.items():
+            # Retrieve other node
+            other_node = other.nodes[path]
+
+            # Compare node types and names
+            if node.type != other_node.type or node.name != other_node.name:
+                return False
+            
+            # Checks for GroupNodes
+            if is_groupnode(node) and is_groupnode(other_node):
+                # Compare attributes
+                if dict(node.attributes) != dict(other_node.attributes):
+                    return False
+            
+            # Checks for DataNodes
+            if is_datanode(node) and is_datanode(other_node):
+                # Compare attributes
+                if dict(node.attributes) != dict(other_node.attributes):
+                    return False
+                    
+                # Compare actual data
+                if not torch.equal(node.data, other_node.data):
+                    return False
+        return True

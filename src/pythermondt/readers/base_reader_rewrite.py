c2ee2e210047e6b1a9788126719cd7108e785a8b
Julfried <julian.grimm8@gmail.com>
2025-03-03 10:57:42 +0100

    Add abstract property paths_prefix and implement files method in BaseReader

--- src/pythermondt/readers/base_reader_rewrite.py
+++ src/pythermondt/readers/base_reader_rewrite.py
@@ -20,6 +20,15 @@ class BaseReader(ABC):
         self.__parser = parser
 
     @property
+    @abstractmethod
+    def paths_prefix(self) -> str:
+        """String that is prepended to all file paths.
+
+        Needed to distinguihs between different readers once they are combined in a single dataset.
+        """
+        raise NotImplementedError("The method must be implemented by the subclass!")
+
+    @property
     def backend(self) -> BaseBackend:
         return self.__backend
 
@@ -27,5 +36,12 @@ class BaseReader(ABC):
     def parser(self) -> BaseParser:
         return self.__parser
 
+    @property
+    def files(self) -> list[str]:
+        return self.backend.get_file_list(pattern=".*", extensions=None)
+
     def __str__(self):
         return f"{self.__class__.__name__}(backend={self.backend.__class__.__name__}, parser={self.parser.__name__}"
+
+    def read_file(self, file_path: str):
+        return self.parser.parse(self.backend.read_file(file_path))

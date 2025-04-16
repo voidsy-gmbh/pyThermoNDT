d75484ff7d553a46ed4897ea29e18c5ccf3c8d67
Julfried <julian.grimm8@gmail.com>
2025-04-16 10:19:12 +0200

    Add basic integration tests for thermodataset

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 
+from pythermondt.data import ThermoDataset
 from pythermondt.readers import LocalReader
 
 from ..utils import containers_equal
@@ -18,7 +19,6 @@ TEST_IDS = [case.id for case in TEST_CASES]
 def test_local_reader_integration(test_case: IntegrationTestCase):
     """Test LocalReader integration using source files and expected outputs."""
     # Create readers
-    print(test_case.source_path)
     source_reader = LocalReader(test_case.source_path)
     expected_reader = LocalReader(test_case.expected_path)
 
@@ -27,4 +27,23 @@ def test_local_reader_integration(test_case: IntegrationTestCase):
         # Compare containers
         assert containers_equal(source_container, expected_container), (
             f"Test case '{test_case.id}': {source_reader.files[i]} and {expected_reader.files[i]} are not equal"
+        )
+
+
+@pytest.mark.parametrize("test_case", TEST_CASES, ids=TEST_IDS)
+def test_thermodataset_integration(test_case: IntegrationTestCase):
+    """Test ThermoDataset integration using source files and expected outputs."""
+    # Create readers
+    source_reader = LocalReader(test_case.source_path)
+    expected_reader = LocalReader(test_case.expected_path)
+
+    # Create ThermoDataset objects
+    source_dataset = ThermoDataset(source_reader)
+    expected_dataset = ThermoDataset(expected_reader)
+
+    # Compare all containers in the dataset
+    for i, (source_container, expected_container) in enumerate(zip(source_dataset, expected_dataset, strict=True)):
+        # Compare containers
+        assert containers_equal(source_container, expected_container), (
+            f"Test case '{test_case.id}': {source_dataset.files[i]} and {expected_dataset.files[i]} are not equal"
         )

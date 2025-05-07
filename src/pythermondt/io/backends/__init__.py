552a60342e0ff09de47e6ae1a2a84c5a9a143fa7
Julfried <julian.grimm8@gmail.com>
2025-05-07 17:08:55 +0200

    Export S3Backend

--- src/pythermondt/io/backends/__init__.py
+++ src/pythermondt/io/backends/__init__.py
@@ -1,4 +1,5 @@
 from .base_backend import BaseBackend
 from .local_backend import LocalBackend
+from .s3_backend import S3Backend
 
-__all__ = ["BaseBackend", "LocalBackend"]
+__all__ = ["BaseBackend", "LocalBackend", "S3Backend"]

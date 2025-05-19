ea60f2ae6155025c460658d39027fdbb70045528
Julfried <julian.grimm8@gmail.com>
2025-05-19 14:02:13 +0200

    Add simple config to pythermondt

--- src/pythermondt/__init__.py
+++ src/pythermondt/__init__.py
@@ -1,9 +1,29 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 from .__pkginfo__ import __version__
 from .data import ThermoContainer, ThermoDataset
 from .io import HDF5Parser, SimulationParser
 from .readers import LocalReader, S3Reader
 from .transforms import augmentation, normalization, preprocessing, sampling, utils
 from .writers import LocalWriter, S3Writer
+
+
+class Settings(BaseSettings):
+    """Global settings for pyThermoNDT."""
+
+    cache_base: str = "."
+
+    model_config = SettingsConfigDict(
+        env_prefix="PYTHERMONDT_",
+        case_sensitive=False,
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="allow",
+    )
+
+
+# Global settings instance
+settings = Settings()
 
 __all__ = [
     "__version__",
@@ -20,6 +40,7 @@ __all__ = [
     "utils",
     "LocalWriter",
     "S3Writer",
+    "settings",
 ]
 
 # TODO: Implement logging instead of print statements according to this guide: https://docs.python.org/3/howto/logging.html

aa9522f8601f6c368d0dd29d20045f6006b027db
Julfried <julian.grimm8@gmail.com>
2025-05-20 14:25:18 +0200

    Fix code style

--- src/pythermondt/readers/base_reader.py
+++ src/pythermondt/readers/base_reader.py
@@ -7,8 +7,8 @@ from tqdm.auto import tqdm
 
 from ..config import settings
 from ..data import DataContainer
-from ..io.parsers import PARSER_REGISTRY, BaseParser, find_parser_for_extension
 from ..io import IOPathWrapper
+from ..io.parsers import PARSER_REGISTRY, BaseParser, find_parser_for_extension
 
 
 class BaseReader(ABC):

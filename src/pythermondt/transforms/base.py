570efadbb800174f119bb6d8ed04990d1ed6d024
Julfried <51880314+Julfried@users.noreply.github.com>
2025-05-31 16:24:30 +0200

    Move Base class Thermotransform to base.py

---
+++ src/pythermondt/transforms/base.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+from torch import nn
+
+from ..data import DataContainer
+
+
+class ThermoTransform(nn.Module, ABC):
+    """Abstract base class that all deterministic transforms of PyThermoNDT must inherit from.
+
+    Initializes the module and sets up necessary configurations.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def forward(self, container: DataContainer) -> DataContainer:
+        """Abstract method that must be implemented in the sub-class.
+
+        This method should contain the actual transformation logic.
+        """
+        raise NotImplementedError("Forward method must be implemented in the sub-class.")
+
+    # Add type annotations to __call__ method, so that the type checker can infer the correct return type.
+    # Otherwise, the return type will be inferred as 'Any'.
+    # __call__ does not have to be overridden, this is already implemented in the nn.Module class from PyTorch.
+    # See: https://github.com/microsoft/pyright/issues/3249
+    def __call__(self, container: DataContainer) -> DataContainer:
+        """Override to provide proper type hints."""
+        return super().__call__(container)
--- src/pythermondt/transforms/utils.py
+++ src/pythermondt/transforms/utils.py
@@ -1,34 +1,5 @@
-from abc import ABC, abstractmethod
-
-import torch.nn as nn
-
 from ..data import DataContainer
-
-
-class ThermoTransform(nn.Module, ABC):
-    """Abstract base class that all transforms of PyThermoNDT must inherit from.
-
-    Initializes the module and sets up necessary configurations.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    @abstractmethod
-    def forward(self, container: DataContainer) -> DataContainer:
-        """Abstract method that must be implemented in the sub-class.
-
-        This method should contain the actual transformation logic.
-        """
-        raise NotImplementedError("Forward method must be implemented in the sub-class.")
-
-    # Add type annotations to __call__ method, so that the type checker can infer the correct return type.
-    # Otherwise, the return type will be inferred as 'Any'.
-    # __call__ does not have to be overridden, this is already implemented in the nn.Module class from PyTorch.
-    # See: https://github.com/microsoft/pyright/issues/3249
-    def __call__(self, container: DataContainer) -> DataContainer:
-        """Override to provide proper type hints."""
-        return super().__call__(container)
+from .base import ThermoTransform
 
 
 class Compose(ThermoTransform):

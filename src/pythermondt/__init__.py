89b178b318f50405e1793079b74796db8cfb920a
Julfried <julian.grimm8@gmail.com>
2025-06-03 11:21:37 +0200

    Update TODOs

--- src/pythermondt/__init__.py
+++ src/pythermondt/__init__.py
@@ -25,5 +25,3 @@ __all__ = [
 # TODO: Implement logging instead of print statements according to this guide: https://docs.python.org/3/howto/logging.html
 # TODO: Implement async data loading
 # TODO: Implement multi threading for data loading / writing
-# TODO: Add more tests
-# TODO: Add glob patterns to S3Reader

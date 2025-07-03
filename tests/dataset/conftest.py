88772947102c5030738b54580ebca3e9166d383c
Julfried <julian.grimm8@gmail.com>
2025-07-03 11:17:43 +0200

    Fix local_reader fixture description and add sample_dataset_simple_transform fixture

--- tests/dataset/conftest.py
+++ tests/dataset/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 
 from pythermondt import LocalReader, ThermoDataset
+from pythermondt.data.datacontainer.core import DataContainer
+from pythermondt.transforms import ThermoTransform
 
 
 @pytest.fixture
 def local_reader():
-    """Fixture for a LocalReader with no files."""
+    """Fixture for a LocalReader with 3 files."""
     return LocalReader(pattern="./tests/assets/integration/simulation/", num_files=3)
 
 
@@ -19,3 +21,26 @@ def sample_dataset_single_file(localreader_with_file: LocalReader):
 def sample_dataset_three_files(local_reader: LocalReader):
     """Create a sample ThermoDataset with multiple files."""
     return ThermoDataset(local_reader)
+
+
+@pytest.fixture
+def single_transform():
+    """Create a simple ThermoTransform that adds an attribute."""
+
+    class SimpleTransform(ThermoTransform):
+        def forward(self, container: DataContainer) -> DataContainer:
+            if "transformed" in container.get_all_attributes("/MetaData"):
+                i = container.get_attribute("/MetaData", "transformed")
+                i = i if isinstance(i, int) else 0
+                container.update_attribute("/MetaData", "transformed", i + 1)
+            else:
+                container.add_attribute("/MetaData", "transformed", 1)
+            return container
+
+    return SimpleTransform()
+
+
+@pytest.fixture
+def sample_dataset_simple_transform(local_reader: LocalReader, single_transform: ThermoTransform):
+    """Create a sample ThermoDataset with a simple transform."""
+    return ThermoDataset(local_reader, transform=single_transform)

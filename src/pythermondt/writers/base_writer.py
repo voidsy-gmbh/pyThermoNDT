8affc90c082cf3841192252d4d63937ed0ab66a0
Julfried <51880314+Julfried@users.noreply.github.com>
2025-08-31 09:54:18 +0200

    Prepare BaseWriter to use Backends

--- src/pythermondt/writers/base_writer.py
+++ src/pythermondt/writers/base_writer.py
@@ -1,13 +1,10 @@
 from abc import ABC, abstractmethod
 
 from ..data import DataContainer
+from ..io.backends import BaseBackend
 
 
 class BaseWriter(ABC):
-    @abstractmethod
-    def __init__(self):
-        """Constructor for the BaseWriter class. Should be called by all subclasses."""
-
     @abstractmethod
     def write(self, container: DataContainer, file_name: str):
         """Actual implementation of the writing a single DataContainer to the destination folder.
@@ -17,3 +14,13 @@ class BaseWriter(ABC):
             destination_folder (str): The destination folder to write to.
             file_name (str): The name of the DataContainer.
         """
+        raise NotImplementedError("Subclasses must implement this method")
+
+    @abstractmethod
+    def _create_backend(self) -> BaseBackend:
+        """Create a new backend instance.
+
+        This method must be implemented by subclasses to create or
+        recreate their backend when needed or after unpickling.
+        """
+        raise NotImplementedError("Subclasses must implement this method")

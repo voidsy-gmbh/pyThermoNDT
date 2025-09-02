efd93aeef63ab68d5f697318dc93e6cf5bca531f
Julfried <julian.grimm8@gmail.com>
2025-09-02 14:48:12 +0200

    Update S3Writer to use backends

--- src/pythermondt/writers/s3_writer.py
+++ src/pythermondt/writers/s3_writer.py
@@ -1,78 +1,41 @@
 import boto3
-from botocore.exceptions import ClientError
-from tqdm.auto import tqdm
 
 from ..data import DataContainer
+from ..io import IOPathWrapper, S3Backend
 from .base_writer import BaseWriter
 
 
 class S3Writer(BaseWriter):
-    def __init__(self, bucket: str, destination_folder: str, boto3_session: boto3.Session | None = None):
-        """Instantiates a new HDF5Writer.
+    def __init__(self, bucket: str, prefix: str, region_name: str | None = None, profile_name: str | None = None):
+        """Instantiates a new instance of the S3Writer class.
 
         Args:
             bucket (str): The name of the bucket to write to.
-            destination_folder (str): The destination folder where the DataContainers should be written to.
-            boto3_session (boto3.Session, optional): The boto3 session to be used for the S3 client.
+            prefix (str): The prefix (folder path) within the bucket to write to.
+            region_name (str | None, optional): The AWS region to use. Defaults to None.
+            profile_name (str | None, optional): The AWS profile to use. Defaults to None.
                 Default is a new boto3 session with the default profile.
         """
+        super().__init__()
+
         # Use default boto3 session if none is provided
-        if not boto3_session:
-            boto3_session = boto3.Session()
+        self.__bucket = bucket
+        self.__prefix = prefix
+        self.__region_name = region_name
+        self.__profile_name = profile_name
 
-        self.bucket = bucket
-        self.destination_folder = destination_folder
-
-        # Create a new s3 client from the give session
-        self.__client = boto3_session.client("s3")
+    def _create_backend(self) -> S3Backend:
+        session = boto3.Session(
+            region_name=self.__region_name,
+            profile_name=self.__profile_name,
+        )
+        return S3Backend(self.__bucket, self.__prefix, session)
 
     def write(self, container: DataContainer, file_name: str):
-        # Specify the destination path
-        if self.destination_folder:
-            path = "/".join([self.destination_folder, file_name])
+        # Append file extension if not present
+        if not file_name.endswith(".hdf5"):
+            file_name += ".hdf5"
 
-        else:
-            path = file_name
-
-        # Serialize the DataContainer to a HDF5 file
-        hdf5_buffer = container.serialize_to_hdf5()
-
-        # Progress bar for uploading the file
-        pbar = tqdm(
-            total=hdf5_buffer.getbuffer().nbytes,
-            desc=f"Uploading file: {file_name}",
-            unit="B",
-            unit_scale=True,  # Scale to MB
-            unit_divisor=1024,  # Convert bytes to MB
-            miniters=1,  # Update progress bar every 1 iteration
-            leave=True,  # Set to False if you don't want the bar to persist after completion
-        )
-
-        # Callback for progress bar
-        class ProgressCallback:
-            def __init__(self, progress_bar: tqdm):
-                self.progress_bar = progress_bar
-
-            def __call__(self, bytes_amount):
-                self.progress_bar.update(bytes_amount)
-
-        # Try to upload the file
-        try:
-            self.__client.upload_fileobj(hdf5_buffer, self.bucket, path, Callback=ProgressCallback(pbar))
-            pbar.close()  # Close the progress bar
-
-        except ClientError as e:
-            error_code = e.response["Error"]["Code"]
-            if error_code in [
-                "InvalidAccessKeyId",
-                "SignatureDoesNotMatch",
-                "AuthFailure",
-                "InvalidSecurity",
-                "InvalidToken",
-            ]:
-                raise PermissionError("Invalid AWS credentials") from e
-            if error_code == "AccessDenied":
-                raise PermissionError("Access denied. Check your AWS permissions for this resource.") from e
-            if error_code == "NoSuchBucket":
-                raise FileNotFoundError(f"The bucket '{self.bucket}' does not exist") from e
-            raise RuntimeError(f"Failed to upload file: {e}") from e
+        # Write the DataContainer to the file
+        data = container.serialize_to_hdf5()
+        self.backend.write_file(IOPathWrapper(data), file_name)

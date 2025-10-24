608b8a92154167b26fe044509a750974b2ac59c9
Julfried <julian.grimm8@gmail.com>
2025-10-24 13:08:40 +0200

    Replace prints with logging

--- src/pythermondt/dataset/thermo_dataset.py
+++ src/pythermondt/dataset/thermo_dataset.py
@@ -1,4 +1,5 @@
 from collections.abc import Iterator, Sequence
+from logging import getLogger
 
 import torch
 
@@ -6,6 +7,8 @@ from ..data import DataContainer
 from ..readers.base_reader import BaseReader
 from ..transforms.utils import _BaseTransform
 from .base import BaseDataset
+
+logger = getLogger(__name__)
 
 
 class ThermoDataset(BaseDataset):
@@ -86,7 +89,7 @@ class ThermoDataset(BaseDataset):
                 for reader in readers_objects:
                     # Check if the reader has found any files
                     if not reader.files:
-                        print(f"No files found for reader of type {reader_type.__qualname__}")
+                        logger.warning(f"No files found for reader of type {reader_type.__qualname__}")
 
                     # Check for duplicate files
                     reader_files = set(reader.files)
@@ -97,12 +100,14 @@ class ThermoDataset(BaseDataset):
                     all_files.update(reader_files)
 
                 if duplicate_files:
-                    print(f"Duplicate files found for reader of type {reader_type.__qualname__}: \n {duplicate_files}")
+                    logger.warning(
+                        f"Duplicate files found for reader of type {reader_type.__qualname__}: \n {duplicate_files}"
+                    )
 
             # Else duplicates are not possible ==> Check if the reader has found any files
             else:
                 if len(readers[0].files) == 0:
-                    print(f"No files found for reader of type {reader_type.__qualname__}")
+                    logger.warning(f"No files found for reader of type {reader_type.__qualname__}")
 
     def _build_index(self):
         """Build an index map using 2 torch Tensors.

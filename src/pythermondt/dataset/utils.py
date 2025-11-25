c2f3404756e2714fa554bfee2bf829ce0f2e45b0
Julfried <julian.grimm8@gmail.com>
2025-11-25 09:07:40 +0100

    Replace logging with warning in random_split

--- src/pythermondt/dataset/utils.py
+++ src/pythermondt/dataset/utils.py
@@ -1,6 +1,6 @@
 import itertools
-import logging
 import math
+import warnings
 from collections.abc import Callable, Sequence
 from functools import partial
 
@@ -11,8 +11,6 @@ from ..data import DataContainer
 from ..transforms import ThermoTransform
 from .indexed_thermo_dataset import IndexedThermoDataset
 from .thermo_dataset import ThermoDataset
-
-logger = logging.getLogger(__name__)
 
 
 def random_split(
@@ -80,7 +78,7 @@ def random_split(
     # Print a warning if any of the splits have a length of 0
     for i, length in enumerate(lengths):
         if length == 0:
-            logger.warning(f"Length of split at index {i} is 0. This might result in an empty dataset.")
+            warnings.warn(f"Length of split at index {i} is 0. This might result in an empty dataset.", stacklevel=2)
 
     # Raise an error if the computed lengths don't match the length of the original dataset
     if sum(lengths) != len(dataset):

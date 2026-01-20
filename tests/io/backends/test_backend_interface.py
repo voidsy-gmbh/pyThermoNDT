5ad17291f3c216a13777afa318dbb5c3f5fbb198
Julfried <julian.grimm8@gmail.com>
2026-01-20 14:26:07 +0100

    Compare test files in binary format

--- tests/io/backends/test_backend_interface.py
+++ tests/io/backends/test_backend_interface.py
@@ -1,14 +1,29 @@
 from pythermondt.io import IOPathWrapper
 
 
-def test_remote_source(configured_backend):
-    backend, config = configured_backend
+def test_remote_source(backend):
+    backend, config = backend
     assert backend.remote_source == config.is_remote
 
 
-def test_read_file(configured_backend, test_files):
-    backend, _ = configured_backend
+def test_read_file(backend, test_file):
+    backend, _ = backend
+    name, content = test_file
 
-    result = backend.read_file(test_files["sample.txt"])
+    result = backend.read_file(name)
     assert isinstance(result, IOPathWrapper)
-    assert result.file_obj.read().decode() == "test content"
+    assert result.file_obj.read() == content
+
+
+def test_read_write_file(backend, tmp_path):
+    backend, _ = backend
+
+    # Write
+    data = IOPathWrapper(b"new test content")
+    file_path = str(tmp_path / "new_sample.txt")
+    backend.write_file(data, file_path)
+
+    # Read back
+    result = backend.read_file(file_path)
+    assert isinstance(result, IOPathWrapper)
+    assert result.file_obj.read() == b"new test content"

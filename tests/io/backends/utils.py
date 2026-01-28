8de7dfee0214868bce312b98525d8a78bb127078
Julfried <julian.grimm8@gmail.com>
2026-01-28 08:55:17 +0100

    Add MockedBlobStorage for testing

--- tests/io/backends/utils.py
+++ tests/io/backends/utils.py
@@ -10,3 +10,36 @@ class TestConfig:
     backend_cls: type[BaseBackend]
     scheme: str
     is_remote: bool
+
+
+class MockAzureBlob:
+    """Mock Azure Blob Storage for testing."""
+
+    def __init__(self):
+        self.storage = {}  # {container: {blob_name: bytes}}
+
+    def create_container(self, container_name: str):
+        if container_name not in self.storage:
+            self.storage[container_name] = {}
+
+    def upload_blob(self, container: str, blob_name: str, data: bytes):
+        if container not in self.storage:
+            self.storage[container] = {}
+        self.storage[container][blob_name] = data
+
+    def download_blob(self, container: str, blob_name: str) -> bytes:
+        return self.storage[container][blob_name]
+
+    def blob_exists(self, container: str, blob_name: str) -> bool:
+        return container in self.storage and blob_name in self.storage[container]
+
+    def list_blobs(self, container: str, prefix: str = "") -> list[str]:
+        if container not in self.storage:
+            return []
+        blobs = self.storage[container].keys()
+        if prefix:
+            blobs = [b for b in blobs if b.startswith(prefix)]
+        return list(blobs)
+
+    def get_blob_size(self, container: str, blob_name: str) -> int:
+        return len(self.storage[container][blob_name])

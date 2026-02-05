22fbcc56277de076f1aa481003c5d5e7a5c0e122
Julfried <julian.grimm8@gmail.com>
2026-02-05 13:17:33 +0100

    Add prototype implementation for device transfer transform

---
+++ src/pythermondt/transforms/device.py
@@ -0,0 +1,25 @@
+from ..data.datacontainer import DataContainer
+from ..data.datacontainer.utils import is_datanode
+from .base import ThermoTransform
+
+
+class DeviceTransfer(ThermoTransform):
+    """A transform that moves all the datasets inside a datacontainer to a specific device."""
+
+    def __init__(self, datasets: list[str] | None = None, device: str = "cpu"):
+        """Initialize the DeviceTransfer transform with specified datasets.
+
+        Args:
+            datasets (list[str]): List of dataset identifiers to be used in the transform.
+            device (str): The target device to which datasets will be moved.
+        """
+        super().__init__()
+        self.datasets = datasets
+        self.device = device
+
+    def forward(self, container: DataContainer) -> DataContainer:
+        for path in container.get_all_dataset_paths() if self.datasets is None else self.datasets:
+            node = container.nodes[path]
+            if is_datanode(node):
+                node.data = node.data.to(self.device)
+        return container

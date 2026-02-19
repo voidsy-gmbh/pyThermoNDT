b1f9c3c261bd86b7df53a1ba273510f088a5a931
Julfried <julian.grimm8@gmail.com>
2026-02-19 12:52:09 +0100

    Make docstring 1 line

--- src/pythermondt/readers/base_reader.py
+++ src/pythermondt/readers/base_reader.py
@@ -175,8 +175,7 @@ class BaseReader(ABC):  # pylint: disable=too-many-instance-attributes
 
     def __setstate__(self, state: dict):
         """Restore object from pickled state."""
-        # Just restore the state dictionary - backend will be created
-        # lazily when first accessed
+        # Just restore the state dictionary - backend will be created lazily when first accessed
         self.__dict__.update(state)
 
         # Recreate the lock

bd2a1678d4c63b33df4c3fd57fd09e38ad732e23
Julfried <julian.grimm8@gmail.com>
2026-02-26 16:01:35 +0100

    Also export EdevisParser in io module

--- src/pythermondt/io/__init__.py
+++ src/pythermondt/io/__init__.py
@@ -1,11 +1,12 @@
 from .backends import AzureBlobBackend, BaseBackend, LocalBackend, S3Backend
-from .parsers import BaseParser, HDF5Parser, SimulationParser
+from .parsers import BaseParser, EdevisParser, HDF5Parser, SimulationParser
 from .utils import IOPathWrapper
 
 __all__ = [
     "BaseParser",
     "HDF5Parser",
     "SimulationParser",
+    "EdevisParser",
     "IOPathWrapper",
     "BaseBackend",
     "AzureBlobBackend",

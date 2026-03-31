4ac31fc7457e4fc3eab234ccc885af44c5db93ba
Julian Grimm <51880314+Julfried@users.noreply.github.com>
2026-03-31 13:20:30 +0200

    Use forkserver for lazy cache manager on Linux to fix multiprocessing deprecation warnings (#369)
    * Switch to forkserver instead of fork on linux to fix deprecation warning
    
    * Update imports
    
    * Cleanup imports
    
    * Update comment according to review

--- src/pythermondt/dataset/base_dataset.py
+++ src/pythermondt/dataset/base_dataset.py
@@ -1,9 +1,9 @@
 import collections
 import copy
 import gc
+import multiprocessing as mp
 import sys
 from abc import ABC, abstractmethod
-from multiprocessing import Manager
 from multiprocessing.managers import ListProxy, SyncManager
 from multiprocessing.pool import ThreadPool
 from typing import Literal
@@ -200,8 +200,9 @@ class BaseDataset(Dataset, ABC):
             else:
                 self.__cache = [self._load_cache_item(i) for i in tqdm(range(num), desc=desc, unit=unit)]
         elif mode == "lazy":
-            # Store the manager so it can be released in case
-            self.__manager = Manager()
+            # Choose context based on platform to avoid deprecated 'fork' in multithreaded processes (Python 3.12+):
+            ctx = mp.get_context("forkserver" if sys.platform == "linux" else "spawn")
+            self.__manager = ctx.Manager()
             # Create a shared list for lazy loading using a list proxy
             self.__cache = self.__manager.list([None] * len(self))
         else:

1d54e1f04f01a0b7f41b211467a9450dc276c5e8
Julfried <julian.grimm8@gmail.com>
2026-05-07 17:34:03 +0200

    remove file uris from state

--- src/pythermondt/readers/base_reader.py
+++ src/pythermondt/readers/base_reader.py
@@ -187,6 +187,7 @@ class BaseReader(ABC):  # pylint: disable=too-many-instance-attributes
 
         # Clear files cache to force reloading
         state["_BaseReader__files"] = None
+        state["_BaseReader__file_uris"] = None
         state["_BaseReader__file_names"] = None
         return state
 

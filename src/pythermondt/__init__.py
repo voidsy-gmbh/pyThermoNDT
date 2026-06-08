9362617285d8b18bea9ae854cbf15149a5ae02e5
Julian Grimm <51880314+Julfried@users.noreply.github.com>
2026-06-08 17:15:59 +0200

    Export Azure classes and frequency module in global __init__ (#424)

--- src/pythermondt/__init__.py
+++ src/pythermondt/__init__.py
@@ -5,15 +5,17 @@ from .config import configure_logging, settings
 from .data import DataContainer, ThermoContainer
 from .dataset import IndexedThermoDataset, ThermoDataset
 from .io import HDF5Parser, SimulationParser
-from .readers import LocalReader, S3Reader
-from .transforms import augmentation, normalization, preprocessing, sampling, utils
-from .writers import LocalWriter, S3Writer
+from .readers import AzureBlobReader, LocalReader, S3Reader
+from .transforms import augmentation, frequency, normalization, preprocessing, sampling, utils
+from .writers import AzureBlobWriter, LocalWriter, S3Writer
 
 # Set up logging per Python best practices: https://docs.python.org/3/howto/logging.html
 # Add NullHandler to prevent "No handlers could be found" warnings
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = [
+    "AzureBlobReader",
+    "AzureBlobWriter",
     "DataContainer",
     "HDF5Parser",
     "IndexedThermoDataset",
@@ -27,6 +29,7 @@ __all__ = [
     "__version__",
     "augmentation",
     "configure_logging",
+    "frequency",
     "normalization",
     "preprocessing",
     "sampling",

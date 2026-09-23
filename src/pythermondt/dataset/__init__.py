c1c800021e786a6adb349fda33b2e1d6de890c7f
Julfried <julian.grimm8@gmail.com>
2026-09-23 13:53:35 +0200

    Export derive and DeriveField from dataset package

--- src/pythermondt/dataset/__init__.py
+++ src/pythermondt/dataset/__init__.py
@@ -1,10 +1,12 @@
 from .indexed_thermo_dataset import IndexedThermoDataset
 from .thermo_dataset import ThermoDataset
-from .utils import container_collate, random_split
+from .utils import DeriveField, container_collate, derive, random_split
 
 __all__ = [
+    "DeriveField",
     "IndexedThermoDataset",
     "ThermoDataset",
     "container_collate",
+    "derive",
     "random_split",
 ]
